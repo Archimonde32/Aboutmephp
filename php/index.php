4f2d70c7c39d8e26883eae7262dffa5842392b17
Archimonde32 <dkgaming718@gmail.com>
2023-06-06 16:25:42 +0200

    miseaj

--- php/index.php
+++ php/index.php
@@ -1,4 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-// phpinfo();
-echo "SALUUUUTTT!!!";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./css/base.css">
+</head>
+
+<body>
+    <header>
+        <p><strong>Developpeur Web et Web Mobile</strong></p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit omnis explicabo hic accusamus, impedit neque.</p>
+    </header>
+
+    <nav class="topnav">
+        <a href="index.html">Accueil</a>
+        <a href="about-me.html">à Propos de moi</a>
+        <a href="my-dreams.html">Mes rêves d'apprenant</a>
+        <a href="my-passion.html">Mes passions</a>
+        <a href="CV.pdf" target="_blank">Mon CV</a>
+    </nav>
+<section>
+    <?php 
+    if(){
+
+    }else{
+        
+    }
+    ?>
+    </section>
+    <footer>
+        <p>©Création pour un projet de formation de DWWM à l'AFCI</p>
+    </footer>
+</body>
+
+</html>
